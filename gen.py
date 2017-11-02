1e1c9a46b341e232f3f174c13f962bff0aafbaf5
RSHAW <scrudgey@gmail.com>
2017-11-02 11:08:14 -0700

    now inserts shortcaptions as necessary

--- gen.py
+++ gen.py
@@ -10,6 +10,22 @@ output_path = '/Users/RSHAW/science/my_papers/dissertation/tex/chapter3.tex'
 plot_hook = re.compile('includegraphics')
 ack_hook = re.compile(r'\\acknowledgments{')
 ack_end_hook = re.compile('}')
+plotname_hook = re.compile('(\w+)\.pdf}')
+
+short_caps = {
+  'colormag': 'Color-magnitude diagram for the high-redshift SpARCS cluster sample',
+  'colormag_dustsub': 'Dust-corrected color-magnitude diagram for the high-redshift SpARCS cluster sample',
+  'sfr_model': 'Toy model of star formation history for a quenched galaxy',
+  'ellipses_clustering': 'Criteria for classifying star-forming, intermediate, and quiescent galaxies based on cuts in dust-corrected color-magnitude space',
+  'quenching_timescales': 'Quenching timescale as a function of redshift',
+  'sfh': 'Toy model of star formation history of a quenched galaxy',
+  'rgb': 'Relationship between cluster mass accretion rate and counts of galaxies in different phases of evolution',
+  'fractions': 'Evolution of passive fraction within the quenching model',
+  'shechter': 'Evolution of the field quiescent fraction with redshift',
+  'UVJ': 'Mean binned galaxy ages in rest-frame \textit{UVJ} space'
+  }
+
+
 
 def gen():
   """Generate new chapter .tex file from paper .tex file.
@@ -61,13 +77,22 @@ def gen():
     line = re.sub(r'\\appendix', r'% \\appendix', line)
     if not skipping:
       if plot_hook.search(line):
-        print 'plot found: {}'.format(line)
+        # print 'plot found: {}'.format(line)
         line = re.sub('plots/', 'figures/c2/', line)
       if ack_hook.search(line):
-        print "found acknowledgements. skipping... "
+        # print "found acknowledgements. skipping... "
         print line
         skipping = True
         continue
+      if plotname_hook.search(line):
+        filename = plotname_hook.search(line).group(1)
+        print 'found plot {}'.format(filename)
+        capstring = 'caption[{}]'.format(short_caps[filename])
+      if '\caption' in line:
+        print 'caption found, subbing {}'.format(capstring)
+        # capstring = 'caption[{}]'.format(short_caps.pop(-1))
+        line = re.sub(r'caption', capstring, line)
+        pass
       outfile.write(line)
     else:
       if ack_end_hook.search(line):
